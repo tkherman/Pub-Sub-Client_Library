192d4fa348de9a7f71ec76dfe42b94df0696f798
John Joseph McGuinness III <jmcguin1@student02.cse.nd.edu>
2017-10-09 19:01:30 -0400

    changed includes

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -2,11 +2,12 @@
  * Implementation of the Client class
  */
 
+#include "client.h"
 #include <iostream>
 #include <string>
 #include <cstring>
 
-void publish(const char* topic, const char* message, size_t length) {
+void Client::publish(const char* topic, const char* message, size_t length) {
 	std::string msg = "PUBLISH ";
 	msg.append(topic);
 	msg += " ";
