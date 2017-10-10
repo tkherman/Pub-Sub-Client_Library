c21bfb13a9562ab4b07292c0a168f7bedc9b27f5
John Joseph McGuinness III <jmcguin1@student02.cse.nd.edu>
2017-10-09 23:07:32 -0400

    adding connection function

--- include/ps_client/macros.h
+++ include/ps_client/macros.h
@@ -2,6 +2,9 @@
 
 #pragma once
 
+#define debug(M, N) \
+    if(DEBUG == true) std::cerr << "[DEBUG]    " << std::setprecision(5) <<getPreciseTime() - start_time << "\t\t" << basename(const_cast<char*>(__FILE__)) << ":" << __func__ << ":" << __LINE__ << "\t\t" << M << N << std::endl
+
 #define PTHREAD_CHECK(f) \
     if ((rc = (f)) != 0) { \
         std::cerr << strerror(rc) << std::endl; \
