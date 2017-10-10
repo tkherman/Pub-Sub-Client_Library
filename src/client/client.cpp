9460d798c266cc936da45126a1811f0bae9cda08
John Joseph McGuinness III <jmcguin1@student02.cse.nd.edu>
2017-10-09 22:19:38 -0400

    what I have for now - on to socketing

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -7,6 +7,14 @@
 #include <string>
 #include <cstring>
 
+Client::Client(const char *host, const char *port, const char *cid) {
+	this->host = host;
+	this->nonce = cid;
+	this->port = port;
+
+	//TODO - figure out what else to put here
+}
+
 void Client::publish(const char* topic, const char* message, size_t length) {
 	std::string msg = "PUBLISH ";
 	msg.append(topic);
@@ -15,8 +23,35 @@ void Client::publish(const char* topic, const char* message, size_t length) {
 	msg += "\n";
 	msg.append(message);
 
-	std::cout << msg << std::endl;
 	send_queue.push(msg);
 }
 
+void Client::subscribe(const char *topic, Callback *callback) {
+	std::string msg = "SUBSCRIBE ";
+	msg.append(topic);
+	msg.append('\n');
+	send_queue.push(msg);
+}
 
+void Client::unsubscribe(const char *topic) {
+	std::string msg = "UNSUBSCRIBE ";
+	msg.append(topic);
+	msg.append('\n');
+	send_queue.push(msg);
+}
+
+void Client::disconnect() {
+	std::string msg = "DISCONNECT ";
+	//TODO - figure out what the USER ID is
+	send_queue.push(msg);
+}
+
+void Client::run() {
+//TODO - NEED THREADS FUNCTIONS
+
+}
+
+void Client::shutdown() {
+//TODO see if slack helps
+
+}
